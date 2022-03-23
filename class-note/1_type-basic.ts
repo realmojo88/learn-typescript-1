96795508a896c2ce0ee1939fb83f93ac66659d6f
ㅇㅇ <realmojo88@gmail.com>
2022-03-24 01:36:59 +0900

    변수 알기
    튜플, 객체, 진위값

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -7,3 +7,16 @@ const num: number = 123;
 const arr: Array<Number> = [1, 2, 3];
 const heroes: Array<String> = ["apple", "banana", "melon"];
 const items: number[] = [1, 2, 3];
+
+// TS 튜플
+const addr: [string, number] = ["gangnam", 23];
+
+// TS 객체
+const obj: object = {};
+let person: { name: string; age: number } = {
+  name: "jung",
+  age: 23,
+};
+
+// TS 진위값
+const isShow: boolean = true;
