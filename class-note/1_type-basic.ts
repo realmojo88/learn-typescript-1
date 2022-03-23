fec71756f45054e9f4f8d06fee07cc960e25d62f
ㅇㅇ <realmojo88@gmail.com>
2022-03-24 01:31:06 +0900

    typescript 선언 방법
    문자열, 숫자, 배열

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,9 @@
+// JS 문자열 선언
+// var str = "hello";
+
+// TS 문자열 선언
+const str: string = "hello";
+const num: number = 123;
+const arr: Array<Number> = [1, 2, 3];
+const heroes: Array<String> = ["apple", "banana", "melon"];
+const items: number[] = [1, 2, 3];
