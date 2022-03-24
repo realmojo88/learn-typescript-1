a3b67ceca781ae23b57bc3e0a6cd92d651444159
realmojo88 <strikers1999@naver.com>
2022-03-24 12:54:44 +0900

    함수를 정의하는 방식

---
+++ class-note/2_functions.ts
@@ -0,0 +1,15 @@
+// 함수의 파라미터의 타입을 정의하는 방식
+// function sum(a: number, b: number) {
+//   return a + b;
+// }
+
+console.log(sum(10, 20));
+
+// 함수의 반환값에 타입을 정의하는 방식
+function add(): number {
+  return 10;
+}
+
+function sum(a: number, b: number): number {
+  return a + b;
+}
